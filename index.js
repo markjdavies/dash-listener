013300a59676f3b86b96972230ca54eccace9598
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-05-08 22:15:49 +0100

    Init and placeholder endpoints

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+// Listen on port 5000
+app.listen(port, () => {
+    console.log(`Server is booming on port 5000
+Visit http://localhost:5000`);
+});
+
+
+// Home route
+app.get("/ping", (req, res) => {
+    res.send("I'm here");
+});
+
+// Mock APIs
+app.get("/press/:buttonName", (req, res) => {
+    const buttonName = req.params.buttonName;
+    res.send(`You pressed ${buttonName}`);
+});
